d9157013c5c6a7da8675084fbff2364e57506d2a
Ramon Leopoldo <ramon.jl@outlook.com>
2025-02-09 21:06:00 -0300

    Ajuste de commands

--- student/management/commands/populate_students.py
+++ student/management/commands/populate_students.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from student.models import Student, GENDER_CHOICES, CLASS_DAYS_CHOICES, BILLING_METHOD_CHOICES, LEVEL_CHOICES, STATUS_CHOICES  # Substitua 'myapp' pelo nome real do seu app
+from student.models import Student, GENDER_CHOICES, CLASS_DAYS_CHOICES, BILLING_METHOD_CHOICES, LEVEL_CHOICES, STATUS_CHOICES
 from payment.models import PaymentPending
 import random
 from decimal import Decimal
@@ -14,6 +14,7 @@ class Command(BaseCommand):
     help = "Popula o banco de dados com 50 estudantes para testes e cria pagamentos pendentes"
 
     def handle(self, *args, **kwargs):
+        # Cria uma lista de estudantes
         students = []
         for _ in range(50):
             student = Student(
@@ -29,18 +30,22 @@ class Command(BaseCommand):
                 level=random_choice(LEVEL_CHOICES),
                 due_date=random.randint(1, 28),
                 status=random_choice(STATUS_CHOICES),
-                status_payment="pendente",
+                status_payment="pendente",  # Definindo o status_payment como 'pendente'
                 notes="Aluno gerado automaticamente para testes."
             )
             students.append(student)
         
+        # Salva os estudantes no banco de dados usando bulk_create
         created_students = Student.objects.bulk_create(students)
         
+        # Cria os pagamentos pendentes apenas para estudantes com status_payment = 'pendente'
         payments = [
             PaymentPending(student=student)
             for student in created_students if student.status_payment == 'pendente'
         ]
+        
+        # Salva os pagamentos pendentes no banco de dados usando bulk_create
         PaymentPending.objects.bulk_create(payments)
         
+        # Mensagem de sucesso
         self.stdout.write(self.style.SUCCESS("Banco de dados populado com 50 estudantes e pagamentos pendentes criados."))
-
